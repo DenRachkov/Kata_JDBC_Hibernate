33d7a42a50123819b2266434a739950687df6655
DenRachkov <105989204+DenRachkov@users.noreply.github.com>
2024-04-16 23:22:45 +0300

    finish jdbc

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Denis", "Ivanov", (byte) 20);
+        userService.saveUser("Ivan", "Pupkin", (byte) 25);
+        userService.saveUser("Sergei", "Petrov", (byte) 31);
+        userService.saveUser("Petr", "Kruglov", (byte) 38);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,49 @@
 package jm.task.core.jdbc.service;
 
+import com.mysql.cj.xdevapi.PreparableStatement;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
 
     }
 
     public void dropUsersTable() {
-
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+        userDaoJDBC.saveUser(name, lastName, age);
+        System.out.println("User c именем - " + name + " добавлен в базу данных");
+
     }
 
     public void removeUserById(long id) {
-
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = userDaoJDBC.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        return users;
+
     }
 
     public void cleanUsersTable() {
+        userDaoJDBC.cleanUsersTable();
 
     }
 }
